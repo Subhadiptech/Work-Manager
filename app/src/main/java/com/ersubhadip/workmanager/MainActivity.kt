5557bc6b6cf04cbea96e034fd901f31ae93a77a3
Subhadiptech <2020ugee055@nitjsr.ac.in>
2022-02-11 04:17:09 +0530

    added input output features

--- app/src/main/java/com/ersubhadip/workmanager/MainActivity.kt
+++ app/src/main/java/com/ersubhadip/workmanager/MainActivity.kt
@@ -2,13 +2,21 @@ package com.ersubhadip.workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import android.widget.Toast.LENGTH_LONG
 import androidx.lifecycle.Observer
 import androidx.work.Constraints
+import androidx.work.Data
 import androidx.work.OneTimeWorkRequest
 import androidx.work.WorkManager
 import com.ersubhadip.workmanager.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
+    companion object{
+        const val INPUT_KEY="key"
+    }
 
     lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,14 +35,23 @@ class MainActivity : AppCompatActivity() {
         val instance = WorkManager.getInstance(applicationContext)
         val constraints = Constraints.Builder().setRequiresCharging(true).build()  //Work manager with Constraints
 
+        val input = Data.Builder().putInt(INPUT_KEY,200).build()        //Work manager with input data
+
+
         val oneTimeWorkRequest:OneTimeWorkRequest=OneTimeWorkRequest.Builder(WorkerClass::class.java)
             .setConstraints(constraints)
+            .setInputData(input)
             .build()
 
         instance.enqueue(oneTimeWorkRequest)
 
         instance.getWorkInfoByIdLiveData(oneTimeWorkRequest.id).observe(this, Observer {
             binding.statusTv.text = it.state.name
+
+            if(it.state.isFinished){
+                val show = it.outputData.getString(WorkerClass.OUTPUT_KEY)  //getting output String Data
+                Toast.makeText(applicationContext,show,LENGTH_LONG).show()
+            }
         })
 
     }
--- app/src/main/java/com/ersubhadip/workmanager/WorkerClass.kt
+++ app/src/main/java/com/ersubhadip/workmanager/WorkerClass.kt
@@ -2,21 +2,34 @@ package com.ersubhadip.workmanager
 
 import android.content.Context
 import android.util.Log
+import androidx.work.Data
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import java.text.SimpleDateFormat
+import java.util.*
 
 class WorkerClass(context: Context,params:WorkerParameters):Worker(context,params) {
+
+    companion object{
+        const val OUTPUT_KEY = "key_output"
+    }
     override fun doWork(): Result {
 
         try {
 
-            for (i in 0..100000){
+            val stop = inputData.getInt(MainActivity.INPUT_KEY,0)
+
+            for (i in 0 until stop){
 
                 Log.i("MY_TAG","Success")
             }
 
+            val format = SimpleDateFormat("dd/mm/yyyy hh:mm:ss")
 
-            return Result.success()
+            val data = format.format(Date())
+            val output = Data.Builder().putString(OUTPUT_KEY,data).build() //sending work output in given format
+
+            return Result.success(output) //sending data
 
 
         }catch (e:Exception){
